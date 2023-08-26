1460a04724386c43f103084225b811e25494a232
Eravar <pinghowng@hotmail.com>
2023-08-27 04:01:32 +0800

    ui not working

--- js/nfclogic.js
+++ js/nfclogic.js
@@ -1,182 +1,133 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // const nfcButton = document.getElementById('nfc-button');
-  // const scanButton = document.getElementById('nfc-button-scan');
-  // const displayOutput = document.getElementById('display-output');
-  // const displayError = document.getElementById('display-error');
-  // const displayError2 = document.getElementById('display-error2');
-  // const displayOutputScan = document.getElementById('display-output-scan');
 
-  const nfcReader = new NDEFReader();
+  const displayError = document.getElementById('display-error');
+  const displayOutput = document.getElementById('display-output');
+  const nfcButton = document.getElementById('write-nfc');
+  const productList = document.getElementById('product-list');
 
-  nfcReader.addEventListener('reading', event => {
-    const {message} = event;
-    const textDecoder = new TextDecoder('utf-8');
-    for (const record of message.records) {
-      const plainText = textDecoder.decode(record.data);
-      // displayOutputScan.innerHTML += (plainText);
-      //query the db api with the id for full info
-      // return a json object with the info
-      // {
-      //  id: 1,
-      //  name: "name",
-      //  price: 1.99,
-      // }
-  }
-  });
-
-  // scanButton.addEventListener('click', async () => {
-  //   try {
-  //     const nfcPermission = await navigator.permissions.query({ name: 'nfc' });
-
-  //     if (nfcPermission.state === 'granted') {
-  //       await startScan();
-  //     } else if (nfcPermission.state === 'prompt') {
-  //       const result = await navigator.permissions.request({ name: 'nfc' });
-
-  //       if (result.state === 'granted') {
-  //         await startScan();
-  //       } else {
-  //         console.log('NFC permission denied.');
-  //       }
-  //     } else {
-  //       console.log('NFC permission denied.');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error requesting NFC permission:', error);
-  //   }
-  // });
-
-  // async function startScan() {
-  //   try {
-  //     await nfcReader.scan();
-  //     console.log('NFC permissions granted');
-  //     // Now you can start reading NFC data
-  //   } catch (error) {
-  //     displayError2.innerHTML = error;
-  //     console.error('Error scanning NFC:', error);
-  //   }
-  // }
-
-  // async function fetchProducts() {
-  //   try {
-  //     // wait for cors error resolve from Benedict
-  //     const response = await axios.get('https://dashboard-tau-ivory.vercel.app/api/products');
-  //     return response.data;
-  //   } catch (error) {
-  //     console.error('Error fetching products:', error);
-  //     displayError.innerHTML += ("\n" + error);
-  //     throw error;
-  //   }
-  // }
-
-  // fetchProducts().then((products) => {
-  //   console.log(products);
-  //   displayOutput.innerHTML = products;
-  //   nfcButton.addEventListener('click', async () => {
-  //     try {
-  //       const nfcPermission = await navigator.permissions.query({ name: 'nfc' });
-
-  //       if (nfcPermission.state === 'granted') {
-  //         await writeNFCData();
-  //       } else if (nfcPermission.state === 'prompt') {
-  //         const result = await navigator.permissions.request({ name: 'nfc' });
-
-  //         if (result.state === 'granted') {
-  //           await writeNFCData();
-  //         } else {
-  //           console.log('NFC permission denied.');
-  //         }
-  //       } else {
-  //         console.log('NFC permission denied.');
-  //       }
-  //     } catch (error) {
-  //       console.error('Error requesting NFC permission:', error);
-  //     }
-  //   })
-  // }
-
-  // );
-
-  // scanButton.addEventListener('click', async () => {
-  //   try {
-  //     const nfcPermission = await navigator.permissions.query({ name: 'nfc' });
-
-  //     if (nfcPermission.state === 'granted') {
-  //       await readNFCData();
-  //     } else if (nfcPermission.state === 'prompt') {
-  //       const result = await navigator.permissions.request({ name: 'nfc' });
-
-  //       if (result.state === 'granted') {
-  //         await readNFCData();
-  //       } else {
-  //         console.log('NFC permission denied.');
-  //       }
-  //     } else {
-  //       console.log('NFC permission denied.');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error requesting NFC permission:', error);
-  //   }
-  // });
-  document.getElementById('startNFCButton').addEventListener('click', async () => {
+  
+  nfcButton.addEventListener('click', async () => {
     try {
-      await nfcReader.scan();
-      console.log('NFC permissions granted');
-      // Now you can start reading/writing NFC data
+      const nfcPermission = await navigator.permissions.query({ name: 'nfc' });
+
+      if (nfcPermission.state === 'granted') {
+        displayOutput.innerHTML += 'NFC permission granted'; 
+        await writeNFCData();
+      } else if (nfcPermission.state === 'prompt') {
+        const result = await navigator.permissions.request({ name: 'nfc' });
+
+        if (result.state === 'granted') {
+          await writeNFCData();
+        } else {
+          displayError.innerHTML += 'NFC permission denied.';
+          console.log('NFC permission denied.');
+        }
+      } else {
+        displayError.innerHTML += 'NFC permission denied.';
+
+        console.log('NFC permission denied.');
+      }
     } catch (error) {
-      console.error('Error requesting NFC permissions:', error);
+      displayError.innerHTML += 'NFC permission denied.';
+
+      console.error('Error requesting NFC permission:', error);
     }
   });
 
-  // async function writeNFCData() {
-  //   try {
-  //     const nfc = new NDEFWriter();
+    async function fetchProducts() {
+      try {
+        const response = await axios.get('https://dashboard-tau-ivory.vercel.app/api/products');
+        return response.data;
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        displayError.innerHTML += ("\n" + error);
+        throw error;
+      }
+    }
 
-  //     // Create an example record to write
-  //     // I'll query the API at this point to grab the data back
-  //     const record = new NDEFRecord({
-  //       recordType: "text",
-  //       data: "id placeholder"
-  //     });
+    async function writeNFCData() {
+      try {
+        const nfc = new NDEFReader();
+        // const products = await fetchProducts();
+        await nfc.write({records:[{data:"2", recordType: "text"}]});
+  
+        displayOutput.innerHTML += 'NFC data written successfully.';
+      } catch (error) {
+        displayError.innerHTML += 'Error writing NFC data: ' + error;
+      }
+    }
+    // Call fetchProducts initially to load product data
+    fetchProducts().then(products => {
+      console.log(products);
+      displayOutput.innerHTML = products;
+      for (const product of products) {
+        var htmlContent = 
+        `
+        <div class="flex flex-row"> <!-- Allow to take up available space -->
+            <div class="flex flex-col">
+              <h2 class="text-black text-2xl font-semibold">
+                Product Name
+              </h2>
+              <p>${product.name}</p>
+              
+              <h3 class="text-black text-xl font-semibold">
+                Product Price
+              </h3>
+              <p>${product.price}</p>
+            </div>
+          </div>
+          <div class="flex flex-col justify-between items-end">
+            <input type="checkbox" id="select" class="flex peer w-4 h-4 mt-2 left-6 accent-blue-600 rounded-full" />
+            <button class="flex text-sky-500 hover:text-sky-700 decoration-solid">
+              View More
+            </button>
+          </div>
+        `
+        const productDiv = document.createElement('div');
+        productDiv.innerHTML = htmlContent;
+        productList.appendChild(productDiv);
+        // nameList.innerHTML += product.name + "<br>";
+        // priceList.innerHTML += product.price + "<br>";
+      }
+      // You can display products information in a meaningful way here
+    });
 
-  //     const message = new NDEFMessage([record]);
-  //     await nfc.write(message);
-
-  //     console.log('NFC data written successfully.');
-  //   } catch (error) {
-  //     console.error('Error writing NFC data:', error);
-  //   }
+  // const nfcReader = new NDEFReader();
+  // nfcReader.addEventListener('reading', event => {
+  //   const {message} = event;
+  //   const textDecoder = new TextDecoder('utf-8');
+  //   for (const record of message.records) {
+  //     const plainText = textDecoder.decode(record.data);
+  //     // displayOutputScan.innerHTML += (plainText);
+  //     //query the db api with the id for full info
+  //     // return a json object with the info
+  //     // {
+  //     //  id: 1,
+  //     //  name: "name",
+  //     //  price: 1.99,
+  //     // }
   // }
+  // });
 
-  // async function readNFCData() {
+  // document.getElementById('startNFCButton').addEventListener('click', async () => {
   //   try {
   //     await nfcReader.scan();
-
-  //     nfcReader.addEventListener('reading', event => {
-  //       const { records } = event;
-  //       displayOutputScan.innerHTML += "test reached";
-  //       for (const record of records) {
-  //         console.log('NFC Record:', record);
-  //         displayOutputScan.innerHTML += (record + "\n");
-  //         // Handle the NFC record data as needed.
-  //       }
-  //     });
-
-  //     console.log('Scanning for NFC tags...');
+  //     console.log('NFC permissions granted');
+  //     // Now you can start reading/writing NFC data
   //   } catch (error) {
-  //     displayError2.innerHTML = error;
-  //     console.error('Error scanning NFC:', error);
+  //     console.error('Error requesting NFC permissions:', error);
+  //   }
+  // });
+
+});
+
+  // async function writeNFCData(productId) {
+  //   try {
+  //     const nfc = new NDEFReader();
+  //     await nfc.write({records:[{data:"1", recordType: "text"}]});
+
+  //     displayOutput.innerHTML += 'NFC data written successfully.';
+  //   } catch (error) {
+  //     displayError.innerHTML += 'Error writing NFC data: ' + error;
   //   }
   // }
-
-  // const nfc = new NDEFReader();
-  // nfc.addEventListener('reading', event => {
-  //     const { records } = event;
-  //       displayOutputScan.innerHTML += "test reached";
-  //       for (const record of records) {
-  //         console.log('NFC Record:', record);
-  //         displayOutputScan.innerHTML += (record+"\n");
-  //         // Handle the NFC record data as needed.
-  //     }
-  // });
-});
