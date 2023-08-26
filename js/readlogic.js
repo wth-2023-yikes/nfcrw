b83364f2c35c467f60f1ff26632d03e77b4cf3a9
Eravar <pinghowng@hotmail.com>
2023-08-27 04:56:24 +0800

    fixes

--- js/readlogic.js
+++ js/readlogic.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const displayOutput = document.getElementById('display-output');
+
     const nfcReader = new NDEFReader();
+    // nfcReader.scan();
+    (async () => {
+        try {
+            await nfcReader.scan();
+            console.error('NFC permissions granted');
+            displayOutput.innerHTML += 'NFC permissions granted';
+            // Now you can start reading/writing NFC data
+        } catch (error) {
+            console.error('Error requesting NFC permissions:', error);
+        }
+    })();
+
     nfcReader.addEventListener('reading', async event => {
         const { message } = event;
+
         const textDecoder = new TextDecoder('utf-8');
         for (const record of message.records) {
             const plainText = textDecoder.decode(record.data);
+            displayOutput.innerHTML += plainText;
             try {
                 const response = await axios.get(`https://dashboard-tau-ivory.vercel.app/api/rfid/${plainText}/product`, {
                     responseType: 'arraybuffer'
